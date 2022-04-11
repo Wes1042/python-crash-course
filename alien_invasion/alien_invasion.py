3b399da793607a9e010922e604122d92b75c5b0e
Wes1042 <wesleytrujillo@uagateway.org>
2022-04-11 19:20:41 -0400

    Game completed

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from game_stats import GameStats
 from button import Button 
 from ship import Ship
 import game_functions as gf
+from scoreboard import Scoreboard
 
 
 
@@ -18,13 +19,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make the Play button.
-
     play_button = Button(ai_settings,screen,"Play")
 
-    # Create an instance to store game statistics.
+    # Create an instance to store game statistics and create a scoreboard .
     stats = GameStats(ai_settings)
     # Make a ship,a group of bullets , and a group of aliens.
     ship = Ship(ai_settings,screen)
+    sb = Scoreboard(ai_settings,screen,stats)
     aliens = Group()
 
     #Create a fleet of aliens.
@@ -34,13 +35,13 @@ def run_game():
 
     #start the main loop for the game
     while True:
-        gf.check_events(ai_settings,screen,ship,bullets) #listener
+        gf.check_events(ai_settings,screen,stats,sb,play_button,ship,aliens,bullets) #listener
         if stats.game_active:
             ship.update() # update ship
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)  # update amount of bullets stored and hit
-            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+            gf.update_bullets(ai_settings, screen, stats,sb,ship, aliens, bullets)  # update amount of bullets stored and hit
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens,bullets)
         
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets,play_button) # display in screen 
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button) # display in screen 
     
  
 run_game()
--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -7,10 +7,15 @@ class GameStats():
         self.reset_stats()
         # Start The game in an inactive state
         self.game_active = False
+        # High score should never be reset.
+        self.high_score = 0
+
 
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ship_left = self.ai_settings.ship_limit 
-
+        self.score = 0
+        self.level = 1
+        
     
